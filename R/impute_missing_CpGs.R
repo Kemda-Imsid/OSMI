ff75ca5fc58cc1090805a84295e606bb9376ab96
Kemda-Imsid <nguedachristelle@yahoo.fr>
2025-04-01 14:59:27 +0200

    update

--- R/impute_missing_CpGs.R
+++ R/impute_missing_CpGs.R
@@ -263,7 +263,7 @@ use_osmi<- function(extendedBetaData,subData,df_annotation,ncores){
 
 use_osmi_island<- function(extendedBetaData,ncores){
 
- # df_annotation<-data.frame(getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19))[,c("chr","strand","Name","pos","Islands_Name")]
+  df_annotation<-data.frame(getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19))[,c("chr","strand","Name","pos","Islands_Name")]
   extendedBetaData<-extendedBetaData[!rownames(extendedBetaData)%in%c("sample_id","tissue"),]
   subData <-cbind("chr" =  df_annotation$chr[match(rownames(extendedBetaData), df_annotation$Name)],
                            "pos" = df_annotation$pos[match(rownames(extendedBetaData), df_annotation$Name)],
