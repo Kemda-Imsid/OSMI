e6fa51051540f3f041ef6d066ba624e003651b06
Kemda-Imsid <nguedachristelle@yahoo.fr>
2025-04-02 13:55:36 +0200

    update

--- R/libraries.R
+++ R/libraries.R
@@ -1,28 +1,28 @@
 
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install(version = "3.20")
-usethis::use_package("BiocManager")
-
-BiocManager::install("minfi", dependencies=TRUE)
-usethis::use_package("minfi")
-BiocManager::install("impute" ,dependencies=TRUE)
-usethis::use_package("impute")
-
-BiocManager::install("methylclockData")
-usethis::use_package("methylclockData")
-
-BiocManager::install("methylclock")
-usethis::use_package("methylclock")
-
-BiocManager::install("IlluminaHumanMethylation450kanno.ilmn12.hg19")
-usethis::use_package("IlluminaHumanMethylation450kanno.ilmn12.hg19")
-# BiocManager::install("methyLImp2")
-# usethis::use_package("methyLImp2")
-
-
-
-
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install(version = "3.20")
+# usethis::use_package("BiocManager")
+#
+# BiocManager::install("minfi", dependencies=TRUE)
+# usethis::use_package("minfi")
+# BiocManager::install("impute" ,dependencies=TRUE)
+# usethis::use_package("impute")
+#
+# BiocManager::install("methylclockData")
+# usethis::use_package("methylclockData")
+#
+# BiocManager::install("methylclock")
+# usethis::use_package("methylclock")
+#
+# BiocManager::install("IlluminaHumanMethylation450kanno.ilmn12.hg19")
+# usethis::use_package("IlluminaHumanMethylation450kanno.ilmn12.hg19")
+# # BiocManager::install("methyLImp2")
+# # usethis::use_package("methyLImp2")
+#
+#
+#
+#
 
 
 
