aa7d0cce34696341d4791435a1b2c7a420b63454
Kemda-Imsid <nguedachristelle@yahoo.fr>
2025-04-02 11:00:17 +0200

    update

--- R/libraries.R
+++ R/libraries.R
@@ -1,38 +1,8 @@
-#install.packages("devtools")
-library(devtools)
-#install.packages("remotes")
-library(remotes)
-# install.packages("ggplot2")
-# install.packages("Metrics")
-# install.packages("missMethods")
-# install.packages("pracma")
-# install.packages("reshape2")
-# install.packages("scales")
-# install.packages("tictoc")
-
-usethis::use_package("ggplot2")
-usethis::use_package("Metrics")
-usethis::use_package("missMethods")
-usethis::use_package("parallel")
-usethis::use_package("pracma")
-usethis::use_package("reshape2")
-usethis::use_package("scales")
-usethis::use_package("tictoc")
-usethis::use_package("utils")
-
 
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install(version = "3.20")
 usethis::use_package("BiocManager")
-
-
-#install.packages("data/methyLImp-master.zip", repos = NULL, type = "source")
-BiocManager::install("pdilena/methyLImp",type="source", dependencies=TRUE)
-# remotes::install_github("pdilena/methyLImp")
-
-#usethis::use_package("methyLImp")
-
 
 BiocManager::install("minfi", dependencies=TRUE)
 usethis::use_package("minfi")
