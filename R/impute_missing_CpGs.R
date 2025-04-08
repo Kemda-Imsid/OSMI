f7d9cec14c7220aad9867b0b30c21c5fa4e8f4d9
Kemda-Imsid <nguedachristelle@yahoo.fr>
2025-04-08 11:54:24 +0200

    Update

--- R/impute_missing_CpGs.R
+++ R/impute_missing_CpGs.R
@@ -335,7 +335,7 @@ use_osmi_island<- function(extendedBetaData,ncores){
 
 impute_missing_CpGs<-function(extendedBetaData,nRows=nRows,nCols=nCols,
                     nRepeat,ncores){
-
+   print("hi")
    df_annotation<-data.frame(minfi::getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19::IlluminaHumanMethylation450kanno.ilmn12.hg19))[,c("chr","strand","Name","pos","Islands_Name")]
    extendedBetaData<-extendedBetaData[!rownames(extendedBetaData)%in%c("sample_id","tissue"),]
    subset<-eliminateMissingsMulti(extendedBetaData)
